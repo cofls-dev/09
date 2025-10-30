8e92b5ff5a0d85cf96cdc6bc7747632715ff3a96
cofls-dev <cofls@sookmyung.ac.kr>
2025-10-30 15:25:04 +0900

    01

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int i;
+	int testscore[5];
+	
+	testscore[0]=10;
+	testscore[1]=20;
+	testscore[2]=30;
+	testscore[3]=40;
+	testscore[4]=50;
+	for (i=0; i<5 ; i++)
+	{
+	printf("grade[%i] = %i\n ", i, testscore[i]);
+}
+	system("PAUSE");
+	return 0;
+}
